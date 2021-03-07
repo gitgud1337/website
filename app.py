a32fc60e89009e3ca3a2ac0a1ef6adf834abc23a
devBro <urrzk1@gmail.com>
2021-03-07 20:02:12 +0100

    new landingPage with intro added.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('page.html')
+    return render_template('withSplash.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,34 @@
+let intro = document.querySelector('.splash');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+
+        logoSpan.forEach((img, idx)=>{
+            setTimeout(()=>{
+                img.classList.add('active');
+            }, (idx + 1) * 666)
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((img, idx)=>{
+
+                setTimeout(()=>{
+                    img.classList.remove('active');
+                    img.classList.add('fade');
+                }, (idx + 1) * 666)
+            })
+        }, 2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 4444);
+
+    });
+});
+
+
+// CURSOR
+$(document).mousemove(function (e) {
+    $('.pointer').css({ left: e.pageX, top: e.pageY });
+})
