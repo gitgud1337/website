c97a517ef8da2a31a3489e4dc665fc23327b7e6a
devBro <urrzk1@gmail.com>
2021-03-30 16:59:28 +0200

    new changes based on customer feedback.

--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,31 @@
 // INTRO
-let intro = document.querySelector('.splash');
-let logoSpan = document.querySelectorAll('.logo');
+// let intro = document.querySelector('.splash');
+// let logoSpan = document.querySelectorAll('.logo');
 
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
+// window.addEventListener('DOMContentLoaded', () => {
+//     setTimeout(() => {
 
-        logoSpan.forEach((img, idx) => {
-            setTimeout(() => {
-                img.classList.add('active');
-            }, (idx + 1) * 777)
-        });
+//         logoSpan.forEach((img, idx) => {
+//             setTimeout(() => {
+//                 img.classList.add('active');
+//             }, (idx + 1) * 777)
+//         });
 
-        setTimeout(() => {
-            logoSpan.forEach((img, idx) => {
-                setTimeout(() => {
-                    img.classList.remove('active');
-                    img.classList.add('fade');
-                }, (idx + 1) * 777)
-            })
-        }, 2000);
+//         setTimeout(() => {
+//             logoSpan.forEach((img, idx) => {
+//                 setTimeout(() => {
+//                     img.classList.remove('active');
+//                     img.classList.add('fade');
+//                 }, (idx + 1) * 777)
+//             })
+//         }, 2000);
 
-        setTimeout(() => {
-            intro.style.top = '-100vh';
-        }, 4444);
+//         setTimeout(() => {
+//             intro.style.top = '-100vh';
+//         }, 4444);
 
-    });
-});
+//     });
+// });
 
 
 // CURSOR
@@ -34,26 +34,26 @@ $(document).mousemove(function (e) {
 })
 
 // MAKE ARTISTS TEXT IN NAVBAR BIGGER ON MOUSEENTER
-let artistButton = document.querySelector('.artistButton');
-let landingCol = document.querySelectorAll('.landingPageCol');
-let card = document.querySelector('.cardRow');
+// let artistButton = document.querySelector('.artistButton');
+// let landingCol = document.querySelectorAll('.landingPageCol');
+// let card = document.querySelector('.cardRow');
 
-artistButton.addEventListener('mousedown', e => {
-    card.classList.add('test');
-})
+// artistButton.addEventListener('mousedown', e => {
+//     card.classList.add('test');
+// })
 
-artistButton.addEventListener('mouseenter', e => {
-    artistButton.classList.add('bigger');
-})
-artistButton.addEventListener('mouseleave', e => {
-    artistButton.classList.remove('bigger');
-})
+// artistButton.addEventListener('mouseenter', e => {
+//     artistButton.classList.add('bigger');
+// })
+// artistButton.addEventListener('mouseleave', e => {
+//     artistButton.classList.remove('bigger');
+// })
 
-artistButton.addEventListener('mousedown', e => {
-    landingCol.forEach((div, divX) => {
-        div.classList.add('remove');
-    });
-})
+// artistButton.addEventListener('mousedown', e => {
+//     landingCol.forEach((div, divX) => {
+//         div.classList.add('remove');
+//     });
+// })
 
 //SCROLL DOWN TO ARTIST COLUMN
 // $(".artistButton").click(function () {
